7966d8a5017638357c6c764c640ad4ccd18a9cf8
dpackham <dpackham@73620c91-2435-7102-8ba9-0d0c51097dca>
2009-02-23 15:53:47 +0000

    Updated infor for Beta

--- src/com/jackcholt/reveal/Global.java
+++ src/com/jackcholt/reveal/Global.java
@@ -2,7 +2,7 @@ package com.jackcholt.reveal;
 
 public class Global {
     public static final String TAG = "reveal";
-	public static int SVN_VERSION = 112;
+	public static int SVN_VERSION = 114;
 	public static int NEW_VERSION;
     
 }
