c7c804d96ac891736bae40792831de7f764dc84c
dpackham <dpackham@73620c91-2435-7102-8ba9-0d0c51097dca>
2009-03-10 15:56:15 +0000

    Threaded UpdateChecker

--- src/com/jackcholt/reveal/UpdateChecker.java
+++ src/com/jackcholt/reveal/UpdateChecker.java
@@ -20,6 +20,9 @@ import android.content.Context;
 import android.webkit.WebView;
 import android.widget.Toast;
 
+
+
+
 /**
  * Checks for updates to the program
  * 
@@ -29,10 +32,10 @@ import android.widget.Toast;
 public class UpdateChecker {
   public static String marketId;
   
-
   @SuppressWarnings("finally")
   public static int getLatestVersionCode(Context _this) {
     int version = 0;
+
     try {
       //Get the XML update Version to prompt user to get a new Update From the market
       FlurryAgent.onEvent("UpdateCheck");
@@ -45,11 +48,21 @@ public class UpdateChecker {
       cnVersion.connect();
       InputStream streamVersion = cnVersion.getInputStream();
   	  //Get the Update location URL
-      WebView wv = new WebView(_this);
-      wv.clearCache(true);
-      wv.getSettings().setJavaScriptEnabled(true);
-      wv.loadUrl("http://revealreader.thepackhams.com/revealUpdate.html");
-     //Proceed parsing the info
+      // I have to use webview here to allow javascript  URL doesnt do javascript
+      WebView mWebView = new WebView(_this);
+      mWebView.clearCache(true);
+      mWebView.getSettings().setJavaScriptEnabled(true);
+      mWebView.loadUrl("http://revealreader.thepackhams.com/revealUpdate.html");
+      //URLConnection cnUpdate;
+      //URL urlUpdate = new URL("http://revealreader.thepackhams.com/revealUpdate.html");
+      //cnUpdate = urlUpdate.openConnection();
+      //cnUpdate.setReadTimeout(10000);
+      //cnUpdate.setConnectTimeout(10000);
+      //cnUpdate.setDefaultUseCaches(false);
+      //cnUpdate.connect();
+      //InputStream streamUpdate = cnUpdate.getInputStream();
+
+      //Proceed parsing the info
       DocumentBuilder docBuild = DocumentBuilderFactory.newInstance().newDocumentBuilder();
       Document manifestDoc = docBuild.parse(streamVersion);
       NodeList manifestNodeList = manifestDoc.getElementsByTagName("manifest");
@@ -78,12 +91,19 @@ public class UpdateChecker {
     }
   }
 
-  public static  void checkForNewerVersion(int currentVersion, Context _this) {
+public static  void checkForNewerVersion(int currentVersion, final Context _this) {
 	  //Check here an XML file stored on our website for new version info
 	  //Comment this out due to feedback fromusers that it checks too much.  at least that it notifies too much
-      //Toast.makeText(_this, R.string.checking_for_new_version_online, Toast.LENGTH_SHORT).show();
-  	  Global.NEW_VERSION = getLatestVersionCode(_this);
-    if (Global.NEW_VERSION > currentVersion) {
+   
+	  Thread t = new Thread() {
+            public void run() {
+            	Global.NEW_VERSION = getLatestVersionCode(_this);
+            }
+      };
+        t.start();
+	  
+	  
+	  if (Global.NEW_VERSION > currentVersion) {
     	//Tell user to Download a new REV of this cool CODE from the Market
     	//Only Toast if there IS and update
         Toast.makeText(_this, R.string.update_available, Toast.LENGTH_LONG).show();
