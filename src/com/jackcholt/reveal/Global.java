c180d17b8aa66535ee9b423fc398a641d76c5faa
dpackham <dpackham@73620c91-2435-7102-8ba9-0d0c51097dca>
2009-03-16 17:56:38 +0000

    181

--- src/com/jackcholt/reveal/Global.java
+++ src/com/jackcholt/reveal/Global.java
@@ -2,6 +2,7 @@ package com.jackcholt.reveal;
 
 public class Global {
     public static final String TAG = "reveal";
-	public static int SVN_VERSION = 177;
+	public static int SVN_VERSION = 181;
 	public static int NEW_VERSION;
+    
 }
