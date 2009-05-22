2f3948a44477c2fd6140fb1a5253078048ab7760
dpackham <dpackham@73620c91-2435-7102-8ba9-0d0c51097dca>
2009-05-22 15:19:55 +0000

    Global Bump

--- src/com/jackcholt/reveal/Global.java
+++ src/com/jackcholt/reveal/Global.java
@@ -2,7 +2,7 @@ package com.jackcholt.reveal;
 
 public class Global {
     public static final String TAG = "reveal";
-	public static int SVN_VERSION = 272;
+	public static int SVN_VERSION = 281;
 	public static int NEW_VERSION;
 	public static int DEBUG = 1;
      
