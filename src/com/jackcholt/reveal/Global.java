f4ec45a11242b31d4ce9adff525a12f862d60511
dpackham <dpackham@73620c91-2435-7102-8ba9-0d0c51097dca>
2009-05-30 22:41:48 +0000

    Build that was uploaded

--- src/com/jackcholt/reveal/Global.java
+++ src/com/jackcholt/reveal/Global.java
@@ -2,7 +2,7 @@ package com.jackcholt.reveal;
 
 public class Global {
     public static final String TAG = "reveal";
-	public static int SVN_VERSION = 282;
+	public static int SVN_VERSION = 296;
 	public static int NEW_VERSION;
 	public static int DEBUG = 1;
      
