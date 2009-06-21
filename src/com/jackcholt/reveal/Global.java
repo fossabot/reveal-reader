17278428c941623ee24650aa8add4d3060e6f85d
dpackham <dpackham@73620c91-2435-7102-8ba9-0d0c51097dca>
2009-06-21 02:31:09 +0000

    POST SNAP build back to Debug mode

--- src/com/jackcholt/reveal/Global.java
+++ src/com/jackcholt/reveal/Global.java
@@ -4,6 +4,6 @@ public class Global {
     public static final String TAG = "reveal";
 	public static int SVN_VERSION = 330;
 	public static int NEW_VERSION;
-	public static int DEBUG = 0;
+	public static int DEBUG = 1;
      
 }
