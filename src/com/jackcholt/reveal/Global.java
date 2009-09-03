02696a10a702036bdcbe3dba1af337b797662092
dpackham <dpackham@73620c91-2435-7102-8ba9-0d0c51097dca>
2009-09-03 18:15:53 +0000

    Changes

--- src/com/jackcholt/reveal/Global.java
+++ src/com/jackcholt/reveal/Global.java
@@ -2,7 +2,7 @@ package com.jackcholt.reveal;
 
 public class Global {
     public static final String TAG = "reveal";
-	public static int SVN_VERSION = 493;
+	public static int SVN_VERSION = 509;
 	public static int NEW_VERSION;
 	public static int DEBUG = 1; // set to 0 for release, 1 for debug, 2 for tracefile creation
      
