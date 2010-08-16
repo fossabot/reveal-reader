8f1fe2aaf62993c4288ea300328472d7ee5cff84
dpackham <dpackham@73620c91-2435-7102-8ba9-0d0c51097dca>
2010-08-16 03:01:36 +0000

    SNAP Build

--- src/com/jackcholt/reveal/Global.java
+++ src/com/jackcholt/reveal/Global.java
@@ -2,8 +2,8 @@ package com.jackcholt.reveal;
 
 public class Global {
     public static final String TAG = "reveal";
-	public static int SVN_VERSION = 682;
+	public static int SVN_VERSION = 683;
 	public static int NEW_VERSION;
-	public static int DEBUG = 1;
+	public static int DEBUG = 0;
 	//0=off,1=on
 }
